e48f54f253830158728525d830fd092b994f99fa
Mateusz Nowak <mateusz00nowak@gmail.com>
2024-12-01 17:53:17 +0100

    Done day 1, part 1

---
+++ day_01/src/Main.java
@@ -0,0 +1,32 @@
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.IntStream;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        Path filePath = Path.of("input.txt");
+
+        List<Integer> firstColumn = new ArrayList<>();
+        List<Integer> secondColumn = new ArrayList<>();
+
+        var lines = Files.readAllLines(filePath);
+        lines.stream()
+                .map(line -> line.trim().split("\\s+"))
+                .forEach(numbers -> {
+                    firstColumn.add(Integer.parseInt(numbers[0]));
+                    secondColumn.add(Integer.parseInt(numbers[1]));
+                });
+
+        firstColumn.sort(Integer::compareTo);
+        secondColumn.sort(Integer::compareTo);
+
+        var totalDistance = IntStream.range(0, firstColumn.size())
+                .map(i -> Math.abs(firstColumn.get(i) - secondColumn.get(i)))
+                .sum();
+
+        System.out.println("Total distance: " + totalDistance);
+    }
+}
