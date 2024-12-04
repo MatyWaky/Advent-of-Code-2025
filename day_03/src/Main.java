35f46f63eeacc1a4d418526ccaf6717eb0eeec42
Mateusz Nowak <mateusz00nowak@gmail.com>
2024-12-04 22:13:21 +0100

    Done day 3, part 2

--- day_03/src/Main.java
+++ day_03/src/Main.java
@@ -7,15 +7,17 @@ public class Main {
 
     public static void main(String[] args) throws Exception {
         firstPart();
+
+        secondPart();
     }
 
-    private static void firstPart() throws Exception{
+    private static void firstPart() throws Exception {
         String text = readFile();
         String regex = "mul\\((-?\\d+),(-?\\d+)\\)";
         Pattern pattern = Pattern.compile(regex);
         Matcher matcher = pattern.matcher(text);
-
         long sum = 0;
+
         while (matcher.find()) {
             long x = Integer.parseInt(matcher.group(1));
             long y = Integer.parseInt(matcher.group(2));
@@ -25,7 +27,31 @@ public class Main {
         System.out.println("Part 1: " + sum);
     }
 
-    private static String readFile() throws Exception{
+    private static void secondPart() throws Exception {
+        String text = readFile();
+        String regex = "(do\\(\\))|(don't\\(\\))|mul\\((-?\\d+),(-?\\d+)\\)";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(text);
+        boolean isEnabled = true;
+        long sum = 0;
+
+        while (matcher.find()) {
+            if (matcher.group(1) != null) {
+                isEnabled = true;
+            } else if (matcher.group(2) != null) {
+                isEnabled = false;
+            } else if (matcher.group(3) != null && matcher.group(4) != null) {
+                if (isEnabled) {
+                    long x = Integer.parseInt(matcher.group(3));
+                    long y = Integer.parseInt(matcher.group(4));
+                    sum += x * y;
+                }
+            }
+        }
+        System.out.println("Part 2: " + sum);
+    }
+
+    private static String readFile() throws Exception {
         Path filePath = Path.of("input.txt");
         return Files.readString(filePath);
     }
